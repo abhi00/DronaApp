7a40be50a80586cc64d1ad1d3ce075303bc6b99c
abhi00 <kabhishek026@gmail.com>
2018-12-01 12:02:57 +0530

    Recent

--- app/src/main/java/com/aptronindia/dronaapp/MainActivity.java
+++ app/src/main/java/com/aptronindia/dronaapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.aptronindia.dronaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
